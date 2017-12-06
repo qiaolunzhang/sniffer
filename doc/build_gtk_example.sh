6a5e01df286f879016124dee7332c9dbca6a133d
qiaolunzhang <qiaolunzhang@gmail.com>
2017-12-06 14:41:17 +0800

    delete unnessa

--- doc/build_gtk_example.sh
+++
@@ -1,1 +0,0 @@
-cc `pkg-config --cflags gtk+-3.0` -o example-0 gtk_example.c `pkg-config --libs gtk+-3.0`
--- doc/error.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("%s", "Hello world\n");  // on stdout
-    fprintf(stdout, "%s", "Hello world\n");  // on stdout
-    fprintf(stderr, "%s", "Stack overflow!\n"); // on stderr
-    return 0;
-}
